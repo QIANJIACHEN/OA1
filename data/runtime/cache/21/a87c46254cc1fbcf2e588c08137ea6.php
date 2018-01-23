d26afbb52bda4035e2a410aade0e6b5be3d643b5
QIANJIACHEN <35715695+QIANJIACHEN@users.noreply.github.com>
2018-01-23 17:14:55 +0800

    Revert "002"
    This reverts commit e01ae266563d214c8b7659d2fcc3dcb69d5521ec.

--- data/runtime/cache/21/a87c46254cc1fbcf2e588c08137ea6.php
+++
@@ -1,4 +0,0 @@
-<?php
-//000000000000
- exit();?>
-a:7:{s:9:"from_name";s:9:"钱佳陈";s:4:"from";s:18:"a52736632a@163.com";s:4:"host";s:12:"smtp.163.com";s:11:"smtp_secure";s:3:"ssl";s:4:"port";s:3:"994";s:8:"username";s:18:"a52736632a@163.com";s:8:"password";s:9:"a52736632";}
--- data/runtime/cache/30/6cdd0f309661b0e0e78338d8e94de3.php
+++
@@ -1,4 +0,0 @@
-<?php
-//000000000000
- exit();?>
-a:4:{s:9:"site_name";s:20:"OA后台测试网站";s:14:"site_seo_title";s:20:"OA后台测试网站";s:17:"site_seo_keywords";s:71:"ThinkCMF,php,内容管理框架,cmf,cms,简约风, simplewind,framework";s:20:"site_seo_description";s:29:"个人用于OA系统的练习";}
